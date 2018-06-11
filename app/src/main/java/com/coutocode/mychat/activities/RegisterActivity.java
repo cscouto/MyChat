1c34b92dc5222d4c36264a147fb3b5d6e8255a7c
Tiago Do Couto <cs.couto@outlook.com>
2018-06-11 16:03:41 -0500

    implemented register activity

--- app/src/main/java/com/coutocode/mychat/activities/RegisterActivity.java
+++ app/src/main/java/com/coutocode/mychat/activities/RegisterActivity.java
@@ -1,22 +1,57 @@
 package com.coutocode.mychat.activities;
 
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import com.coutocode.mychat.FirebaseAPI;
 import com.coutocode.mychat.R;
-
 import java.util.Objects;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity implements FirebaseAPI.FirebaseListener {
+
+    @BindView(R.id.btnRegister)
+    Button btnRegister;
+    @BindView(R.id.etEmail)
+    EditText etEmail;
+    @BindView(R.id.etPassword)
+    EditText etPassword;
+
+    FirebaseAPI api;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        api = new FirebaseAPI();
+        api.listener = this;
+
+        ButterKnife.bind(this);
+
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!etEmail.getText().toString().isEmpty() && !etPassword.getText().toString().isEmpty()){
+                    api.registerUser(etEmail.getText().toString(), etPassword.getText().toString(),
+                            RegisterActivity.this);
+                }else{
+                    AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                    builder.setMessage(R.string.fill_fields).create();
+
+                }
+            }
+        });
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -26,4 +61,21 @@ public class RegisterActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void userCreated() {
+        startActivity(new Intent(this, ChatActivity.class));
+    }
+
+    @Override
+    public void failed(int message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+        builder.setMessage(message).create();
+    }
+
+    @Override
+    public void loggedIn() { }
+
+    @Override
+    public void newMessage() { }
 }
