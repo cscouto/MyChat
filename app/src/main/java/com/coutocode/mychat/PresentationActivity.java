2b894e2a8611c38188b230d1a620ecaaadc13bab
Tiago Do Couto <cs.couto@outlook.com>
2018-06-11 14:58:49 -0500

    implemented presentation activity

--- app/src/main/java/com/coutocode/mychat/PresentationActivity.java
+++ app/src/main/java/com/coutocode/mychat/PresentationActivity.java
@@ -1,13 +1,38 @@
 package com.coutocode.mychat;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class PresentationActivity extends AppCompatActivity {
+
+    @BindView(R.id.btnLogin)
+    Button btnLogin;
+    @BindView(R.id.btnRegister)
+    Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_presentation);
+        ButterKnife.bind(this);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PresentationActivity.this, LoginActivity.class));
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PresentationActivity.this, RegisterActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/coutocode/mychat/RegisterActivity.java
+++ app/src/main/java/com/coutocode/mychat/RegisterActivity.java
@@ -2,6 +2,7 @@ package com.coutocode.mychat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +10,16 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home){
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
